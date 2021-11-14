1348057f9ad20ad295d8f4734db866867392f459
CmcCormack92 <86354138+CmcCormack92@users.noreply.github.com>
2021-11-14 17:48:55 -0500

    add icons and advanced styles

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -42,16 +42,18 @@ var loadTasks = function () {
     tolerance: "pointer",
     helper: "clone",
     activate: function (event) {
-
+      $(this).addClass("dropover")
+      $(".bottom-trash").addClass("bottom-trash-drag")
     },
     deactivate: function (event) {
-
+      $(this).removeClass("dropover")
+      $(".bottom-trash").removeClass("bottom-trash-drag")
     },
     over: function (event) {
-
+      $(event.target).addClass("dropover-active")
     },
     out: function (event) {
-
+      $(event.target).removeClass("dropover-active")
     },
     update: function (event) {
 
@@ -91,12 +93,13 @@ var loadTasks = function () {
     tolerance: "touch",
     drop: function (event, ui) {
       ui.draggable.remove();
+      $(".bottom-trash").removeClass("bottom-trash-active")
     },
     over: function (event, ui) {
-      console.log("over");
+      $(".bottom-trash").addClass("bottom-trash-active");
     },
     out: function (event, ui) {
-      console.log("out");
+      $(".bottom-trash").removeClass("bottom-trash-active");
     }
   })
 
@@ -255,7 +258,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -284,6 +287,12 @@ $("#remove-tasks").on("click", function () {
   }
   saveTasks();
 });
+
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 1800000);
 
 // load tasks for the first time
 loadTasks();
